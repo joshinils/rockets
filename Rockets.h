866efeccfb61b960cbe527b4e77d472622991934
joshinils <joshua.laskowski@web.de>
2019-09-10 04:52:05 +0200

    added demonstration of plane via mandelbrot

--- Rockets.h
+++ Rockets.h
@@ -24,6 +24,24 @@ public:
 		std::cout << __FUNCTION__ << std::endl;
 
 		Clear(olc::BLACK);
+		double minx = minX();
+		double miny = minY();
+		double maxx = maxX();
+		double maxy = maxY();
+
+		for (size_t i = 0; i < olc::PixelGameEngine::ScreenWidth(); i++)
+		{
+			for (size_t j = 0; j < olc::PixelGameEngine::ScreenHeight(); j++)
+			{
+				double x = minx + (maxx-minx) * i / olc::PixelGameEngine::ScreenWidth();
+				double y = miny + (maxy-miny) * j / olc::PixelGameEngine::ScreenHeight();
+
+				size_t amount = mandelbrot(x,y);
+				Draw(x,y,olc::Pixel(amount%255, (amount*8+1)%255, (amount*16+1)%255));
+			}
+		}
+
+
 
 		for (int i = -10; i < 10; i++)
 		{
@@ -33,20 +51,43 @@ public:
 			}
 		}
 		Draw(0, 0 ,olc::GREEN);
-
-		double minx = minX();
-		double miny = minY();
-		double maxx = maxX();
-		double maxy = maxY();
-
-		double r = 10;
-		FillCircle(minx, miny, r, olc::MAGENTA);
-		FillCircle(minx, maxy, r, olc::YELLOW);
-		FillCircle(maxx, miny, r, olc::BLUE);
-		FillCircle(maxx, maxy, r, olc::CYAN);
-
 		return true;
 	}
+
+	const int MAXITERS = 100;
+
+	template <typename zahl>
+	size_t mandelbrot(const zahl& rC, const zahl& iC)
+	{
+		size_t iters = 0;
+		zahl rZ = 0;
+		zahl iZ = 0;
+
+		while (++iters < MAXITERS && rZ*rZ + iZ*iZ < 4)
+		{
+			// malnehmen
+			// Z = (rZ + iZ)^2 + (rC + iC)
+			// Z = (rZ + I*iZ)(rZ + I*iZ) + (rC + I*iC)
+			// Z = rZ*rZ + 2*IiZ*rZ + I^2*iZ^2 + (rC + I*iC)
+			// Z = rZ*rZ + rC - iZ^2   +   2*I*iZ*rZ + I*iC
+
+			static zahl oldrZ = rZ;
+			oldrZ = rZ;
+			// rZ <- rZ*rZ + rC - iZ^2
+
+			rZ = rZ*rZ + rC - iZ*iZ;
+			//rZ *= rZ;
+			//rZ += rC - iZ*iZ;
+
+			// iZ <- 2*iZ*rZ + iC
+			iZ = 2 * iZ*oldrZ + iC;
+			//iZ *= 2*oldrZ;
+			//iZ += iC;
+		}
+
+		return iters;
+	}
+
 
 private:
 
--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 int main()
 {
+	double scale = 40;
 	Rockets demo;
-	if (demo.Construct(1280, 720, 1, 1))
+	if (demo.Construct(16*scale, 9*scale, 120/scale, 120/scale))
 		demo.Start();
 	return 0;
 }
